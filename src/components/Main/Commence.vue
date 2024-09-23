dee6b275cc82ea8d2d0c134484608095d0fd36f1
lauraoliveri <lauraoliveri2003@gmail.com>
2024-09-23 19:16:38 +0200

    Design mains section "Commence"

--- src/components/Main/Commence.vue
+++ src/components/Main/Commence.vue
@@ -10,16 +10,18 @@ export default {
 </script>
 
 <template>
-  <section>
-      <h3>
-        a
-      </h3>
-      <h2>
-        a
-      </h2>
-      <button>
-        a
-      </button>
+  <section class="d-flex align-items-center justify-content-center">
+      <div>
+          <h3>
+            Commence Business
+          </h3>
+          <h2>
+            Distant Mentoring Program
+          </h2>
+          <button type="button" class="btn btn-primary">
+            Download free guidebook &#8594;
+          </button>
+      </div>
   </section>
 </template>
 
@@ -28,6 +30,32 @@ export default {
 
 section {
   height: calc(100vh - 150px);
-  background-color: aquamarine;
+  background-image: url(../../assets/images/home-3-hero-bg.jpg);
+  background-size: cover;
+  color: white;
+  text-align: center;
+
+  h3 {
+    font-size: 30px;
+  }
+
+  h2 {
+    font-size: 60px;
+    padding-bottom: 20px;
+  }
+
+  .btn-primary {
+    background-color: white;
+    color: $MainGreen;
+    padding: 15px 30px;
+    border: 0;
+    font-size: 14px;
+    font-weight: bold;
+
+    &:hover {
+      background-color: $MainGreen;
+      color: white;
+    }
+  }
 }
 </style>
